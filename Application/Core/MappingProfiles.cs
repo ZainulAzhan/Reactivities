0d2c80e4673b7b0373b26c6abfd2b2de82bc6419
Zainul Azhan <zainulazhan2014@gmail.com>
2023-12-07 07:57:47 +0800

    End of section 4

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using AutoMapper;
+
+namespace Application.Core;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Activity, Activity>();
+    }
+}
